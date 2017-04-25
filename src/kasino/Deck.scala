21b8e036c38740b25975bfbc842697876a2d6fdb
Juhana <Juhana@192.168.0.199>
2017-04-25 21:51:23 +0300

    Cleaning and commenting the deck class.

--- src/kasino/Deck.scala
+++ src/kasino/Deck.scala
@@ -10,11 +10,14 @@ class Deck(seed: Int) {
 
   //Contains all the cards in the deck; Turn into private when done testing!
   private var cards = Buffer[Card]()
+  
+  //check if the deck is empty
+  def isEmpty = this.cards.isEmpty
 
   //Contains all the suits. Used by initialize to go through all the suits to make a complete deck.
   private val Suits = Vector[Suit](Spades, Diamonds, Clubs, Hearts)
   
-  //adds a card to the cards buffer
+  //adds a card to the cards collection
   private def addCard(value: Int, Suit: Suit) = {
     cards += new Card(value, Suit)
   }
@@ -22,7 +25,7 @@ class Deck(seed: Int) {
   //adds a collection of cards to the deck, used when loading
   def addCards(in: Seq[Card]) = in.foreach( x => cards += x)
 
-  //Creates a new deck that contains all the suits going ace -> two -> ... -> king
+  //fills the deck so that it contains all the suits going ace -> two -> ... -> king
   //Suits are in order, defined by the Suits-vector
   def initialize() = {
     cards.clear
@@ -40,26 +43,18 @@ class Deck(seed: Int) {
   //deal a certain amount of cards from the deck
   def deal(amount: Int): Buffer[Card] = {
     if (amount > cards.size) { //if there aren't enought cards in the deck, throw an exception
-      throw new IllegalArgumentException("tried to deal too many cards from deck.")
+      throw new IllegalArgumentException("tried to deal too many cards from the deck.")
     }
 
     if (amount < 0) { //if the amount is negative, throw an exception
-      throw new IllegalArgumentException("tried to deal a negative amount of cards from deck.")
+      throw new IllegalArgumentException("tried to deal a negative amount of cards from the deck.")
     }
 
     val dealt = cards.slice(0, amount) //create a new Buffer with the first <amount> cards from the cards-buffer
     cards = cards.drop(amount) //remove <amount> cards from the cards-buffer
     dealt //return the removed cards
   }
-
-  def collectAll(): Buffer[Card] = {
-    val temp = for (i <- cards) yield i
-    cards.clear
-    temp
-  }
   
-  def isEmpty = this.cards.isEmpty
-  
-  //returns all the cards as text in the deck without changing the actual Buffer storing them.
+  //returns all the cards as text in the deck without changing the actual Buffer storing them. Used when saving
   def returnCards = this.cards.map(_.toString)
 }
